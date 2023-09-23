5ee8c08c97733d4fc76bc06f28e3d3008b24a506
IuliiaPi <julia.pidlisna@gmail.com>
2023-09-22 20:29:17 -0400

    feat: add player

--- js/index.js
+++ js/index.js
@@ -4,62 +4,47 @@ console.log(playList);
 
 const listItems = document.querySelector('.list-items');
 const backgroundImage = document.querySelector('.main');
+const btnPlay = document.querySelector('.play');
+
+const audio = new Audio();
+let isPlay = false;
 
 playList.forEach((item, index) => {   // or  for(let i = 0; i < playList.length; i++) { }
     const li = document.createElement('li');
     li.classList.add('item');
-
     li.textContent = item.title;  // or li.textContent = `${playList[index].title}`;
     listItems.append(li);
-    // audio.src = item.src;
 
-    li.addEventListener('click', toggleBtn);
-    // btnPlay.classList.add('play');
     li.addEventListener('click', (e) => {
         backgroundImage.style.backgroundImage = `url(${item.image})`;
-        // li.classList.add('_active');
-        // backgroundImage.style.backgroundImage = "url('./assets/images/solovey.jpg')";
         // backgroundImage.url = playList[index].image;
         // document.body.style.backgroundImage = `url(${playList[index].image})`;
+        btnPlay.classList.add('pause');
+        playAudio();
     });
 
-});
 
-
-// player  
-
-const btnPlay = document.querySelector('.play');
-
-let isPlay = false;
-
-function toggleBtn() {
-    btnPlay.classList.toggle('pause');
-    playAudioPlayer();
-}
-btnPlay.addEventListener('click', toggleBtn);
-
-const audio = new Audio();
-
-function playAudio() {
-    audio.src = "./assets/audio/forest.mp3";
-    // audio.src = item.src;
-    audio.currentTime = 0;
-    audio.play();
-    isPlay = true;
-}
-
-function pauseAudio() {
-    audio.pause();
-    isPlay = false;
-}
-
-function playAudioPlayer() {
-    if (!isPlay) {
-        playAudio();
-    } else {
-        pauseAudio();
+    function playAudio() {
+        audio.src = item.src;
+        audio.currentTime = 0;
+        audio.play();
+        isPlay = true;
     }
-}
+
+    function pauseAudio() {
+        audio.pause();
+        isPlay = false;
+    }
+
+    function playAudioPlayer() {
+        if (!isPlay) {
+            playAudio();
+        } else {
+            pauseAudio();
+        }
+    }
+
+});
 
 
 // change color
@@ -78,15 +63,46 @@ for (let i = 0; i < buttons.length; i++) {
     });
 };
 
+// logo 
 
 const logo = document.querySelector('.logo');
+
 logo.addEventListener('click', (e) => {
     backgroundImage.style.backgroundImage = "url('./assets/images/forest.jpg')";
-    audio.src = "./assets/audio/forest.mp3";
+    btnPlay.classList.add('pause');
+    playAudioPlayer();
+
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].classList.remove('_active')
     };
+
 });
 
 
+// player  
 
+function toggleBtn() {
+    btnPlay.classList.toggle('pause');
+    playAudioPlayer();
+}
+btnPlay.addEventListener('click', toggleBtn);
+
+function playAudio() {
+    audio.src = "./assets/audio/forest.mp3";
+    audio.currentTime = 0;
+    audio.play();
+    isPlay = true;
+}
+
+function pauseAudio() {
+    audio.pause();
+    isPlay = false;
+}
+
+function playAudioPlayer() {
+    if (!isPlay) {
+        playAudio();
+    } else {
+        pauseAudio();
+    }
+}
